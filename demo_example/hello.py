390eccec3ec6d7f615f4272bbbea62ea07bb0ab7
shaojason999 <shaojason999@gmail.com>
2018-12-17 08:28:36 +0000

    First

--- demo_example/hello.py
+++ demo_example/hello.py
@@ -5,4 +5,8 @@ from bottle import route, run
 def hello():
     return "Hello World!"
 
+@route('/message1')
+def hello():
+    return "Hello World!!"
+
 run(host='localhost', port=8080, debug=True)
